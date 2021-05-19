feff7d3a320da430df41acc043dc8527a71b96f3
Andrew Delarge <cryenginemail@gmail.com>
2021-05-19 14:30:40 +0400

    Init empty project

---
+++ Assets/External/CodeLib/Singltone.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace CodeLib
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+        private static T s_Instance;
+        private static object m_Lock = new object();
+        private static bool s_ApplicationStopping = false;
+
+        public static T Instance() {
+            lock (m_Lock) {
+                if (s_Instance == null) {
+                    T[] objects = FindObjectsOfType<T>();
+                    if (objects.Length > 0) {
+                        if (objects.Length > 1) {
+                            Debug.LogError("Singleton: Critical Error! There are more than one instance of" + typeof(T).ToString());
+                        }
+                        s_Instance = objects[0];
+                        Debug.Log("Singleton: Using instance already created: " + s_Instance.gameObject.name);
+                    } else if (!s_ApplicationStopping) {
+                        GameObject singleton = new GameObject();
+                        s_Instance = singleton.AddComponent<T>();
+                        singleton.name = typeof(T).ToString();
+                        Debug.Log("Singleton: An instance of " + typeof(T) + " is needed in the scene, so it was created.");
+                    }
+                }
+                return s_Instance;
+            }
+        }
+
+        protected virtual void OnApplicationQuit() {
+            s_ApplicationStopping = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/AppManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class AppManger : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // load data
+            // init ui
+        }
+    }
+}
